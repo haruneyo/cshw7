98723735fe4b3f4aac7ef956c99b45fba0af173b
leonid ivanov <leonid.ivanov.w@gmail.com>
2022-11-10 02:37:16 +0500

    Task 50 done

---
+++ Task50/Program.cs
@@ -0,0 +1,63 @@
+﻿int[,] array = new int[5, 5];
+FillArray(array, -10, 10);
+System.Console.WriteLine("The array is:");
+PrintArray(array);
+System.Console.WriteLine("Please enter the index (rows, collumns):");
+System.Console.WriteLine(ArrayValueOfIndex(array)); // Returns the value of the given index, if applicable
+System.Console.WriteLine("Please enter the value:");
+System.Console.WriteLine(ArrayIndexOfValue(array)); // Returns the index of the given value, if applicable
+
+void FillArray(int[,] a, int min, int max)
+{
+    for (int i = 0; i < a.GetLength(0); i++)
+    {
+        for (int j = 0; j < a.GetLength(1); j++)
+        {
+            a[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+}
+void PrintArray(int[,] a)
+{
+    for (int i = 0; i < a.GetLength(0); i++)
+    {
+        for (int j = 0; j < a.GetLength(1); j++)
+        {
+            System.Console.Write($"{a[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+string ArrayValueOfIndex(int[,] a)
+{
+    string value = "This index does not exist in the current array";
+    int row = Convert.ToInt32(Console.ReadLine());
+    int col = Convert.ToInt32(Console.ReadLine());
+    for (int i = 0; i < a.GetLength(0); i++)
+    {
+        for (int j = 0; j < a.GetLength(1); j++)
+        {
+            if (row == i && col == j) value = $"{a[i, j]}";
+        }
+    }
+    return value;
+}
+string ArrayIndexOfValue(int[,] a)
+{
+    string index = "This value does not exist in the current array";
+    int value = Convert.ToInt32(Console.ReadLine());
+    for (int i = 0; i < a.GetLength(0); i++)
+    {
+        for (int j = 0; j < a.GetLength(1); j++)
+        {
+            if (value == a[i, j])
+            {
+                index = $"The index of the number {value} is {(i, j)}";
+                goto Flag;
+            }
+            
+        }
+    }
+    Flag: 
+    return index;
+}
